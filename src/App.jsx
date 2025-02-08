b3f91c5262076d46c05e0732d4affffca4d5fa8f
Don Atherton <don@donatherton.co.uk>
2025-02-08 12:25:30 +0000

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 export default function App() {
 
   return(
     <>
-      <Main />
-    </>
+    <Main />
+    <Footer />
+  </>
         
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,19 @@
 import profilePic from '../images/profile.png';
 import logo from '../images/logo.png';
-//import { useState } from 'react';
 
+export default function Main() {
 
-export default function Header() {
-//  const [name, setName] = useState('Your name here');
-
-// function updateName(e) {
-// //    if (e.target.value.length > 1) e.target.value = e.target.value.slice(-1);
-//     setName(e.target.value);
-//   }
-
+  const inputs = [...Array(5)].map((e,i) =>
+    <div className="input-div" key={i}>
+      <input type="text" placeholder="Your attribute here" />
+        <input type="text"
+          className="rating"
+          size="2"
+          maxLength="2"
+          pattern="[0-9]{1,2}" /> 
+        <hr />
+    </div>);
+  
   return(
     <main>
       <div className="stripes" id="top"></div>
@@ -19,59 +22,15 @@ export default function Header() {
       <img src={profilePic} id="profile-pic" alt="Profile picture" />
       <img src={logo} id="logo" alt="logo" />
       <input type="text" id="name" name="name" placeholder="Your name here" />
-      <div id="attr-div">
-      <input type="text" id="attr1" name="attr1" placeholder="Your attribute here" />
-      <input type="text"
-        className="rating"
-        id="rating1"
-        name="rating1"
-        size="2"
-        maxLength="2"
-        pattern="[0-9]{1,2}" /> 
-      <hr />
-      <input type="text" id="attr2" name="attr2" placeholder="Your attribute here" />
-        <input type="text"
-        className="rating"
-        id="rating2"
-        name="rating2"
-        size="2"
-        maxLength="2"
-        pattern="[0-9]{1,2}" />
-      <hr />
-      <input type="text" id="attr3" name="attr3" placeholder="Your attribute here" />
-        <input type="text"
-        className="rating"
-        id="rating3"
-        name="rating3"
-        size="2"
-        maxLength="2"
-        pattern="[0-9]{1,2}" />
-      <hr />
-      <input type="text" id="attr4" name="attr4" placeholder="Your attribute here" />
-        <input type="text"
-        className="rating"
-        id="rating4"
-        name="rating4"
-        size="2"
-        maxLength="2"
-        pattern="[0-9]{1,2}" />
-      <hr />
-      <input type="text" id="attr5" name="attr5" placeholder="Your attribute here" />
-        <input type="text"
-        className="rating"
-        id="rating5"
-        name="rating5"
-        size="2"
-        maxLength="2"
-        pattern="[0-9]{1,2}" />
-      <hr />
+
+      <div id="attr-div"> 
+        {inputs}
       </div>
+
       <div id="fact-file">
         <h2>FACT FILE</h2>
         <textarea name="text-area" placeholder="Say a few words about yourself here" />
       </div>
-      
-
     </main>
   );
 }
